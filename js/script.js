5e3c0e46ce02a13b2cd3f46cf50a6c0edab91e70
Alessandro Marega <ale_marega@hotmail.it>
2021-03-25 16:14:16 +0100

    aggiunto array di immagini in VueJs e aggiunto v-bind a immagine

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const app = new Vue ({
   el: "#root",
   data: {
+    images: [
+      "img/vespucci.jpg",
+      "img/swan.jpg",
+      "img/tp52.jpg",
+      "img/vendee_globe.jpg",
+    ],
+    imgIndex: 0,
 
   },
   methods: {
